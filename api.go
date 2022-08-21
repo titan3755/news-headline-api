e87b55b0516466d25ce1a8a52e99e9fe51f7e526
Titan <68808479+titan3755@users.noreply.github.com>
2022-08-21 15:52:28 +0600

    Updated code

--- api.go
+++ api.go
@@ -1,26 +1,63 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"strconv"
+	"log"
 	"math/rand"
 	"time"
+	"github.com/gin-gonic/gin"
+	"github.com/gocolly/colly"
 )
 
+type Headline struct {
+	Id int `json:"id"`
+	Title string `json:"title"`
+	Sdesc string `json:"sdesc"`
+}
+
 func main() {
-	data := ""
+	data := make([]Headline, 0)
 	go webScraper(&data)
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": data,
+			"message": "Welcome to News-headline scraper API! Visit '/headlines' route to get the responses.",
 		})
 	})
+	r.GET("/headlines", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "List of recent news headlines -->",
+			"data": data,
+		})
+	}) 
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
 
-func webScraper(data *string) { 
-	for range time.NewTicker(time.Second * 1).C { 
-		*data = strconv.Itoa(rand.Int())
+func webScraper(data *[]Headline) { 
+	
+	webScraperLogic := func () {
+		c := colly.NewCollector()
+		c.OnHTML(".zox-s-title2", func (element *colly.HTMLElement) {
+			headlineId := rand.Int()
+			headlineTitle := element.Text
+			headlineDesc := ""
+			headline := Headline{
+				Id: headlineId,
+				Title: headlineTitle,
+				Sdesc: headlineDesc,
+			}
+			*data = append(*data, headline)
+		})
+		c.OnScraped(func (r *colly.Response) {
+			log.Println("Scraped data from website!")
+		})
+		c.Visit("https://boldtv.com/")
 	}
+	webScraperLogic()
+	for range time.NewTicker(time.Minute * 1).C { 
+		webScraperLogic()
+		// enc := json.NewEncoder(os.Stdout)
+		// enc.SetIndent("", " ")
+		// enc.Encode(*data)
+	}
+
 }
